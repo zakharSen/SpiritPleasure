83c4f0d1f391d288f5f8980b4cd43375619c179e
RomanRavlyk <Roman.Ravlyk.RI.2023@lpnu.ua>
2024-04-02 00:57:50 +0300

    Created registration/authorization

--- Back-End/SpiritPleasure/authentication/models.py
+++ Back-End/SpiritPleasure/authentication/models.py
@@ -1,54 +1,11 @@
-# models.py
-
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
 
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None):
-        if not email:
-            raise ValueError('Users must have an email address')
-        if not username:
-            raise ValueError('Users must have a username')
-
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            username=username,
-            password=password,
-        )
-        user.is_admin = True
-        user.is_staff = True
-        user.save(using=self._db)
-        return user
-
-
-class CustomUser(AbstractBaseUser):
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
-    password = models.CharField(max_length=128)
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'username'
-    EMAIL_FIELD = 'email'
-
-    def __str__(self):
-        return self.username
-
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
-
-    def has_module_perms(self, app_label):
-        return True
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
--- Back-End/SpiritPleasure/authentication/views.py
+++ Back-End/SpiritPleasure/authentication/views.py
@@ -1,40 +1,32 @@
-# views.py
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserRegisterSerializer
+from rest_framework.authtoken.models import Token
 
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import CustomUser
-import json
+@api_view(['POST',])
+def logout_user(request):
+    if request.method == "POST":
+        request.user.auth_token.delete()
+        return Response({"message": "You are logged out"}, status=status.HTTP_200_OK)
 
+@api_view(['POST',])
+def user_register_view(request):
+    if request.method == "POST":
+        serializer = UserRegisterSerializer(data=request.data)
 
-@csrf_exempt
-def create_user(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        username = data.get('username')
-        email = data.get('email')
-        password = data.get('password')
+        data = {}
 
-        if username and email and password:
-            user = CustomUser.objects.create_user(username=username, email=email, password=password)
-            return JsonResponse({'message': 'User created successfully'}, status=201)
+        if serializer.is_valid():
+            account = serializer.save()
+
+            data['response'] = 'Account has been created'
+            data['username'] = account.username
+            data['email'] = account.email
+
+            # Create token for the newly registered user
+            token = Token.objects.create(user=account)
+            data['token'] = token.key
         else:
-            return JsonResponse({'error': 'Missing username, email, or password'}, status=400)
-    else:
-        return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
-
-
-def get_user(request, user_id):
-    if request.method == 'GET':
-        try:
-            user = CustomUser.objects.get(id=user_id)
-            user_data = {
-                'id': user.id,
-                'username': user.username,
-                'email': user.email
-            }
-            return JsonResponse(user_data, status=200)
-        except CustomUser.DoesNotExist:
-            return JsonResponse({'error': 'User does not exist'}, status=404)
-    else:
-        return JsonResponse({'error': 'Only GET requests are allowed'}, status=405)
+            data = serializer.errors
+        return Response(data)
