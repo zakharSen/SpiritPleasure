ee572f0c5babc3833aca0cbebd245168b933c2e7
NotOriginal333 <m.naumovych2@gmail.com>
2024-04-02 16:25:29 +0300

    tryDB

--- Back-End/SpiritPleasure/SpiritPleasure/settings.py
+++ Back-End/SpiritPleasure/SpiritPleasure/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'SpiritPleasure.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-# #     'default': {
-# #         'ENGINE': 'django.db.backends.mysql',
-# #         'NAME': 'SpiritPleasure',
-# #         'USER': 'backendmanager',
-# #         'PASSWORD': 'sleep-eat-code-repeat',
-# #         'HOST': 'localhost',
-# #         'PORT': '3306',
-# #     }
-# # }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'SpiritPleasure',
+         'USER': 'backendmanager',
+         'PASSWORD': 'sleep-eat-code-repeat',
+         'HOST': 'localhost',
+         'PORT': '3306',
+     }
+ }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
