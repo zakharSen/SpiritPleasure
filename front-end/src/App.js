6a1967bfe4169bfb01da6ed405790a678731c17d
admin <Senyshyn5@gmail.com>
2024-04-02 19:35:20 +0300

    fonts changing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,17 +10,21 @@ function App() {
         </div>
         <div className="right-pane">
           <div className="top-right"></div>
-          <div className="bottom-right">
-            <div className="input-group">
-              <label className="Name" htmlFor="name">Name</label>
-              <input type="text" id="name"/>
+            <div className="bottom-right">
+                <div className="input-group">
+                    <label className="Name" htmlFor="name">Name</label>
+                    <input type="text" id="name"/>
+                </div>
+                <div className="input-group">
+                    <label className="Email" htmlFor="name">E-mail</label>
+                    <input type="text" id="email"/>
+                </div>
+                <div className="input-group">
+                    <label className="Password" htmlFor="password">Password</label>
+                    <input type="password" id="password"/>
+                </div>
+                <button className="Create-Account-button">Create Account</button>
             </div>
-            <div className="input-group">
-              <label className="Password" htmlFor="password">Password</label>
-              <input type="password" id="password"/>
-            </div>
-            <button className="Create-Account-button">Create Account</button>
-          </div>
         </div>
       </div>
   );
